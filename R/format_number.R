5c191ff8d5ee19d5a735bfe446eab55865413c95
salah <izi31416@protonmail.com>
2026-04-21 20:56:09 -0300

    updated
    alterado decimal_mark para decimal.mark

--- R/format_number.R
+++ R/format_number.R
@@ -12,7 +12,11 @@ count_digits <- function(x) {
         return(0)
       }
       if (abs(x - round(x)) > .Machine$double.eps^0.5) {
-        nchar(strsplit(sub("0+$", "", format(x, scientific = F)), ".", fixed = TRUE)[[1]][[2]])
+        nchar(strsplit(
+          sub("0+$", "", format(x, scientific = F)),
+          ".",
+          fixed = TRUE
+        )[[1]][[2]])
       } else {
         return(0)
       }
@@ -26,7 +30,7 @@ count_digits <- function(x) {
 fnumber <- function(
   x,
   nsmall = NULL,
-  decimal_mark = ",",
+  decimal.mark = ",",
   preffix = NULL,
   suffix = NULL,
   p = FALSE,
@@ -59,7 +63,9 @@ fnumber <- function(
   s <- sign(x)
   x <- abs(x)
 
-  if (is.null(nsmall)) nsmall <- max(count_digits(x))
+  if (is.null(nsmall)) {
+    nsmall <- max(count_digits(x))
+  }
   nn <- 1 / 10^nsmall
 
   if (!is.finite(x)) {
@@ -68,19 +74,26 @@ fnumber <- function(
     x <- round(x * s, nsmall)
     out <- do.call(
       fn,
-      list(x,
+      list(
+        x,
         nsmall = nsmall,
-        decimal_mark = decimal_mark,
+        decimal.mark = decimal.mark,
         scientific = FALSE,
         trim = TRUE,
         ...
       )
     )
 
-    if (p && !parse_p) out <- paste("p =", out)
-    if (p && parse_p) out <- paste0("list(~italic(p)", suffix_p, "=='", out, "')")
+    if (p && !parse_p) {
+      out <- paste("p =", out)
+    }
+    if (p && parse_p) {
+      out <- paste0("list(~italic(p)", suffix_p, "=='", out, "')")
+    }
 
-    if (!is.null(preffix)) out <- paste0(preffix, out)
+    if (!is.null(preffix)) {
+      out <- paste0(preffix, out)
+    }
     if (!is.null(suffix)) out <- paste0(out, suffix)
   } else if (x == 0) {
     out <- "0"
@@ -89,56 +102,76 @@ fnumber <- function(
       if (parse_p) {
         if (p) {
           out <- paste0(
-            "~list(~italic(p)", suffix_p, "~'< ",
-            do.call(fn, list(
-              nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            )), "')"
+            "~list(~italic(p)",
+            suffix_p,
+            "~'< ",
+            do.call(
+              fn,
+              list(
+                nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            ),
+            "')"
           )
         } else {
           out <- paste0(
             "<",
-            do.call(fn, list(
-              nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            do.call(
+              fn,
+              list(
+                nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } # end if
 
-        if (!is.null(preffix)) out <- paste0(preffix, out)
+        if (!is.null(preffix)) {
+          out <- paste0(preffix, out)
+        }
         if (!is.null(suffix)) out <- paste0(out, suffix)
       } else {
         if (p) {
           out <- paste0(
-            paste0("p", suffix_p), " < ",
-            do.call(fn, list(
-              nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            paste0("p", suffix_p),
+            " < ",
+            do.call(
+              fn,
+              list(
+                nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } else {
           out <- paste0(
             "<",
-            do.call(fn, list(
-              nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            do.call(
+              fn,
+              list(
+                nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } # end if
 
-        if (!is.null(preffix)) out <- paste0(preffix, out)
+        if (!is.null(preffix)) {
+          out <- paste0(preffix, out)
+        }
         if (!is.null(suffix)) out <- paste0(out, suffix)
       } # end if
     } # end if
@@ -148,56 +181,75 @@ fnumber <- function(
         if (p) {
           out <- paste0(
             "list(~italic(p)",
-            suffix_p, "~'< ",
-            do.call(fn, list(
-              s * nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ), "')")
+            suffix_p,
+            "~'< ",
+            do.call(
+              fn,
+              list(
+                s * nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              ),
+              "')"
+            )
           )
         } else {
           out <- paste0(
             "<",
-            do.call(fn, list(
-              s * nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            do.call(
+              fn,
+              list(
+                s * nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } # end if
 
-        if (!is.null(preffix)) out <- paste0(preffix, out)
+        if (!is.null(preffix)) {
+          out <- paste0(preffix, out)
+        }
         if (!is.null(suffix)) out <- paste0(out, suffix)
       } else {
         if (p) {
           out <- paste0(
-            paste0("p", suffix_p), " < ",
-            do.call(fn, list(
-              s * nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            paste0("p", suffix_p),
+            " < ",
+            do.call(
+              fn,
+              list(
+                s * nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } else {
           out <- paste0(
             "<",
-            do.call(fn, list(
-              s * nn,
-              decimal_mark = decimal_mark,
-              scientific = FALSE,
-              trim = TRUE,
-              ...
-            ))
+            do.call(
+              fn,
+              list(
+                s * nn,
+                decimal.mark = decimal.mark,
+                scientific = FALSE,
+                trim = TRUE,
+                ...
+              )
+            )
           )
         } # end if
 
-        if (!is.null(preffix)) out <- paste0(preffix, out)
+        if (!is.null(preffix)) {
+          out <- paste0(preffix, out)
+        }
         if (!is.null(suffix)) out <- paste0(out, suffix)
       } # end if
     } # end if
@@ -217,7 +269,7 @@ format_number <- function(
   x,
   nsmall = NULL,
   value = FALSE,
-  decimal_mark = ",",
+  decimal.mark = ",",
   preffix = NULL,
   suffix = NULL,
   p = FALSE,
@@ -245,7 +297,7 @@ format_number <- function(
       x,
       fnumber,
       nsmall = nsmall,
-      decimal_mark = decimal_mark,
+      decimal.mark = decimal.mark,
       preffix = preffix,
       suffix = suffix,
       p = p,
